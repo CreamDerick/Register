4630dc46138c2dd56b13ba906bfefb9e9267d88b
CreamDerick <derickrobles1512@gmail.com>
2024-03-05 14:40:20 +0800

    first commit

---
+++ register.js
@@ -0,0 +1,11 @@
+let txtUsername = document.querySelector("#txtUsername")
+let txtRegister = document.querySelector("#btnRegister")
+
+btnRegister.onclick = function () {
+    register(txtUsername.value)
+}
+
+function register (username) {
+    
+}
+  
