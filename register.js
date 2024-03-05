01c4a0280aad43a439a45e523782eed24a9dab32
CreamDerick <derickrobles1512@gmail.com>
2024-03-05 14:42:51 +0800

    second commit

--- register.js
+++ register.js
@@ -1,11 +1,29 @@
-let txtUsername = document.querySelector("#txtUsername")
-let txtRegister = document.querySelector("#btnRegister")
+let txtUsername = document.querySelector("#txtUsername");
+        let btnRegister = document.querySelector("#btnRegister");
+        let txtPassword = document.querySelector("#txtPassword");
+        let txtConfirmPassword = document.querySelector("#txtConfirmPassword");
+        
+        btnRegister.onclick = function () {
+            register(txtUsername.value, 
+                txtPassword.value, 
+                txtConfirmPassword.value)
+        }
+        function register (username, password, confirmpassword) {
+            // if...else...
+            // conditional operators (greater than >, less than <, equal ==, not !, or ||
+            // greater than or equal >=, less than or equal<=
+            // not equal !=
 
-btnRegister.onclick = function () {
-    register(txtUsername.value)
-}
-
-function register (username) {
-    
-}
-  
+        if(password == confirmpassword && password != "" && confirmpassword != "") {
+                return console.log("Password match!")
+        } else if (password == "" || confirmpassword =="") {
+            return console.log("Password must not be empty")
+        }
+        else {
+            return console.log("Password does not match")
+        }
+            
+        
+        
+            console.log(username, password, confirmpassword)
+        }
